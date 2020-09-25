3f6ee1d018e21c23eeaf30ff88f63d8cb6e6718f
Katsuya Masaka <masaka09021800@gmail.com>
2020-09-25 16:03:28 +0900

    レスポンシブCSS７割

---
+++ HTML/clone/js/main.js
@@ -0,0 +1,7 @@
+'use strict'
+{
+  const title = document.querySelector('header_top > h1');
+  const nav = document.querySelector('nav > li');
+  const sns = document.querySelector('header_sns > a');
+  
+}
